140a5f48fb484f6ff99673ec931368eb7cfbc9fe
A-L-O-K <apvhu03@gmail.com>
2024-06-26 12:00:51 +0530

    Code to create database is done.....

--- database.py
+++ database.py
@@ -1,88 +1,99 @@
+
 import psycopg2
+import os
+from dotenv import load_dotenv
 
-class DataBase:
-    def __init__(self): # initalize the class and establishes connection with the database
-        try:
-            self.conn = psycopg2.connect(
-                database="code_arena",
-                user="postgres",
-                password="icandoit",
-                host="127.0.0.1",
-                port="5432"
-            )
+load_dotenv()
 
-            self.cur = self.conn.cursor()
-            print("Connection succesfully established !")
-        
-        except Exception:
-            print("Error!, Couldnt connect to the database...")
-    
-    def close_connection(self): # function to close the connection with the database
-        try:
-            self.conn.close()
-            print("Connection Closed Successfully !")
-            return 1
-        except Exception:
-            return 0
-    
-    def commit(self): # function to commit in the database to save changes
-        self.conn.commit()
-    
+database = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
 
-    def do_query(self, query):
-        try:
-            self.cur.execute(query)
-            self.commit()
-            
-        except Exception as e:
-            self.conn.rollback()
-            print("An error occurred:", e)
-            return 0
-    
-    def add_question_answer(self, title, description, difficulty, user_id, language, code):
-        try:
+conn = psycopg2.connect(
+    database=database,
+    user=user,
+    password=password,
+    host=host,
+    port=port
+)
 
-            # preprocessing to avoid possible problems.. :)
-            description = description.replace("'", "`")
-            description = description.replace('"', "`")
-            
-            # Start transaction
-            self.conn.autocommit = False
-            
-            # Step 1: Insert into questions and get the question_id
-            insert_question_query = """
-                INSERT INTO questions (title, description, difficulty, solution_id, user_id) 
-                VALUES (%s, %s, %s, %s, %s)
-                RETURNING question_id;
-            """
-            self.cur.execute(insert_question_query, (f'{title}', f'{description}', difficulty, 0, user_id))
-            
-            question_id = self.cur.fetchone()[0]
+cur = conn.cursor()
 
-            # Step 2: Insert into solutions using the retrieved question_id
-            insert_solution_query = """
-                INSERT INTO solutions (language, code, question_id) 
-                VALUES (%s, %s, %s)
-                RETURNING solution_id;
-            """
-            self.cur.execute(insert_solution_query, (f'{language}', f'{code}', question_id))
-            solution_id = self.cur.fetchone()[0]
+# -----------------------------
 
-            # Step 3: Update questions to set the solution_id
-            update_question_query = """
-                UPDATE questions 
-                SET solution_id = %s 
-                WHERE question_id = %s;
-            """
-            self.cur.execute(update_question_query, (solution_id, question_id))
+admins = """
+    CREATE TABLE admins (
+        admin_id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );
+"""
 
-            # Commit the transaction
-            self.conn.commit()
-            print("Data inserted into solutions table successfully!")
+players = """
+    CREATE TABLE players (
+        player_id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        wins_count INTEGER
+    );
+"""
 
-        except Exception as e:
-            # If an error occurs, rollback the transaction
-            self.conn.rollback()
-            print("An error occurred:", e)
+competition = """
+    CREATE TABLE competition (
+        competition_id SERIAL PRIMARY KEY,
+        participant1_id INTEGER NOT NULL,
+        participant2_id INTEGER NOT NULL,
+        question_id INTEGER NOT NULL,
+        start_time TIME NOT NULL,
+        end_time TIME NOT NULL,
+        winner_id INTEGER NOT NULL
+    );
+"""
 
-    
+code_submission = """
+    CREATE TABLE code_submission (
+        submission_id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        competition_id INTEGER NOT NULL,
+        code TEXT,
+        language VARCHAR(255)
+    );
+"""
+
+questions = """
+    CREATE TABLE questions (
+        question_id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        difficulty INTEGER NOT NULL,
+        solution_id INTEGER,
+        user_id INTEGER
+    );
+"""
+
+solutions = """
+    CREATE TABLE solutions (
+        solution_id SERIAL PRIMARY KEY,
+        language VARCHAR(255) NOT NULL,
+        code TEXT NOT NULL,
+        question_id INTEGER
+    );
+"""
+
+
+cur.execute(admins)
+cur.execute(players)
+cur.execute(questions)
+cur.execute(solutions)
+cur.execute(competition)
+cur.execute(code_submission)
+
+
+# -----------------------------
+
+conn.commit()
+cur.close()
